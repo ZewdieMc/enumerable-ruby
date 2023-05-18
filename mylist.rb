b49042dbdb425d4c347498057c2e0db2d02477fe
ZewdieMc <zewdiehabtie26@gmail.com>
2023-05-18 17:40:14 +0300

    Add test cases

--- mylist.rb
+++ mylist.rb
@@ -12,6 +12,18 @@ class MyList
 end
 
 list = MyList.new([1, 2, 3, 4])
-puts(list.all?(&:positive?))
-puts(list.any?(&:negative?))
-puts(list.filter { |item| item > 2 })
+# Test #all?
+puts list.all? {|e| e < 5}
+# true
+puts list.all? {|e| e > 5}
+# false
+
+# Test #any?
+puts list.any? {|e| e == 2}
+# true
+puts list.any? {|e| e == 5}
+# false
+
+# Test #filter
+puts list.filter {|e| e.even?}
+# [2, 4]
